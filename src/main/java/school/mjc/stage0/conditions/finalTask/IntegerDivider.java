961a663bc7328222f730a1e5e70831d9021c78d6
khatia ormotsadze <khatiaormotsadze99@gmail.com>
2023-06-12 15:23:37 +0400

    git commit -m "Modify BitwiseValuesSwap.java" -m "Modify CoordinatePane.java" -m "Modify DaysInMonth.java" -m "Modify IntegerDivider.java" -m "Modify SeasonDeterminer.java" -m "Modify TriangleSidesValidator.java"

--- src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
@@ -2,5 +2,16 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class IntegerDivider {
     public void printCompletelyDivided(int dividend, int divider) {
+
+     double result = dividend / divider;
+     double multiply = result * divider;
+
+     if(multiply == dividend){
+         System.out.println("can be divided completely");
+     } else{
+         System.out.println("cannot be divided completely");
+     }
+
+
     }
 }
